a568086b445f48f8b4c5c24d03d217ddbcdc1c1c
RightL <liu20001021@hotmail.com>
2014-11-23 17:36:48 +0800

    NEw

---
+++ code/SentenceToWords.cpp
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+    char centence[50]={0};
+    char words[20][20]={0};
+    int count_words_num=0;
+    int count_words_letter=0;
+    if (gets(centence)==NULL)
+    {
+        printf("In put ERROR!\n");
+        exit(1);
+    }
+    return 0;
+}
+
