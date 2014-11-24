f6272b838f32e767b96c818600c55ba1045db12a
RightL <liu20001021@hotmail.com>
2014-11-24 14:01:29 +0800

    centencetowords.cpp BUG:only put one word!

--- code/SentenceToWords.cpp
+++ code/SentenceToWords.cpp
@@ -1,22 +1,30 @@
 #include <stdlib.h>
-#include <ctype.h>
 #include <stdio.h>
+#include <cctype>
 int main(void)
 {
-    char centence[50]={0};
-    char words[20][20]={0};
+    char centence[80]={0};
+    char words[20][30]={{0}};
     int count_words_num=0;
     int count_words_letter=0;
+    printf("输入一些单词，用逗号分割：\n");
+    if (fgets(centence,50,stdin)==NULL)
+    {
+        printf("In put error!\n");
+        exit(1);
+    }
     for (int i = 0; i < 50; ++i)
     {
-        if (isalpha(centence[i]))
+        if (isalpha(centence[i])!=0)
         {
             words[count_words_num][count_words_letter]=centence[i];
-            count_words_letter++;
+            ++count_words_letter;
+            continue;
         }
         if (centence[i]==',')
         {
-            count_words_num++;
+            ++count_words_num;
+            printf("%d\n", count_words_num);
         }
     }
     for (int i = 0; i < count_words_num; ++i)
