96e330a1b4f6024f327883eb415241cd59734d44
RightL <liu20001021@hotmail.com>
2014-11-28 14:07:15 +0800

    new code Bar.cpp

---
+++ code/Bar.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define PAGE_HEIGHT 20
+#define PAGE_WIDTH 40
+typedef struct barTAG
+{
+    double value;
+    struct barTAG *pnextbar;
+}bar;
+int main(void)
+{
+    
+    return 0;
+}
+
