621965849d5af7af78d8af9d1a036511b34023e2
RightL <liu20001021@hotmail.com>
2014-12-03 20:35:55 +0800

    fix:some small bugs

--- code/AverageNumber.cpp
+++ code/AverageNumber.cpp
@@ -2,8 +2,22 @@
 #include <cctype>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+struct Lnum
+{
+    struct Lnum *pnext;
+    float f_number; 
+}
 int main(void)
 {
+    double t_f_number;
+    size_t count_number;
+    char in_put[10]={0};
+    while(true)
+    {
+        fgets(in_put,10,stdin);
+        t_f_number=atof(in_put);
+    }
     return 0;
 }
 
