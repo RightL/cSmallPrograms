960f80c67558fe7c27ebef00173c80728dbfca39
RightL <liu20001021@hotmail.com>
2014-12-15 14:07:07 +0800

    Small chance for this file

--- code/PutSentencesLongShort.cpp
+++ code/PutSentencesLongShort.cpp
@@ -11,20 +11,23 @@ struct Node
     struct Node *pLeft;
     struct Node *pRight;
 };
-struct Node *createnode(char string[MAX_LEN]);
+struct Node *createnode(char string[MAX_LEN],int string_lenth);
 int main(void)
 {
     char tmpstring[MAX_LEN]={0};        /*mabey we shouldn't initialize it '*/
+    int tmpstring_lenth;
     struct Node *pRoot=NULL;
     while(true)
     {
+        tmpstring_lenth=0;
         printf("输入一个句子(在新行键入回车退出输入)\n");
         fgets(tmpstring,MAX_LEN,stdin);
         if (tmpstring[0]=='\n')
         {
             break;    
         }
-        pRoot=createnode(tmpstring);
+        tmpstring_lenth=strlen(tmpstring);
+        pRoot=createnode(tmpstring,tmpstring_lenth);
         fflush(stdin);
     }
     return 0;
@@ -38,13 +41,14 @@ struct Node *createnode(char string[MAX_LEN],int string_lenth)
     }
     pNode->count=1;
     pNode->pLeft=pNode->pRight=NULL;
+    pNode->string_lenth=string_lenth;
     return pNode;
 }
-struct Node *addnode(char string[],struct Node *pNode)
+struct Node *addnode(char string[],struct Node *pNode,int string_lenth)
 {
     if (pNode==NULL)
     {
-        return createnode(string);
+        return createnode(string,string_lenth);
     }    
     if ()
     {
