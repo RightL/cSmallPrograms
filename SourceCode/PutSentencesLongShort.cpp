0505535333b6f7756163ebd18450993640be9826
RightL <liu20001021@hotmail.com>
2014-12-19 13:43:27 +0800

    bug:When two sentences with the same lenth,it will only print the first of it

--- SourceCode/PutSentencesLongShort.cpp
+++ SourceCode/PutSentencesLongShort.cpp
@@ -12,25 +12,38 @@ struct Node
     struct Node *pRight;
 };
 struct Node *createnode(char string[MAX_LEN],int string_lenth);
-struct Node *addnode(char string[],int string_lenth);
+struct Node *addnode(char string[],struct Node *pNode,int string_lenth);
+void freenodes(struct Node *pNode);
+void listnodes(struct Node *pNode);
 int main(void)
 {
-    char tmpstring[MAX_LEN]={0};        
     int tmpstring_lenth;
     struct Node *pRoot=NULL;
+    printf("输入一个句子(在新行键入回车退出输入)\n");
     while(true)
     {
         tmpstring_lenth=0;
-        printf("输入一个句子(在新行键入回车退出输入)\n");
+        char tmpstring[MAX_LEN]={0};
         fgets(tmpstring,MAX_LEN,stdin);
         if (tmpstring[0]=='\n')
         {
             break;    
         }
         tmpstring_lenth=(int)strlen(tmpstring);
-        pRoot=createnode(tmpstring,tmpstring_lenth);
+        if(pRoot==NULL)
+        {
+            pRoot=createnode(tmpstring,tmpstring_lenth);
+        }
+        else
+        {
+            addnode(tmpstring,pRoot,tmpstring_lenth);
+        }
+        
         fflush(stdin);
     }
+    printf("句子从小到大输出为：\n");
+    listnodes(pRoot);
+    freenodes(pRoot);
     return 0;
 }
 struct Node *createnode(char string[MAX_LEN],int string_lenth)
@@ -65,7 +78,7 @@ struct Node *addnode(char string[],struct Node *pNode,int string_lenth)
         }
         else
         {
-            return addnode(string,string_lenth);
+            return addnode(string,pNode->pLeft,string_lenth);
         }
     }
     else
@@ -77,11 +90,37 @@ struct Node *addnode(char string[],struct Node *pNode,int string_lenth)
         }
         else
         {
-            return addnode(string,string_lenth);
+            return addnode(string,pNode->pRight,string_lenth);
         }
     }
 }
 
-
+void listnodes(struct Node *pNode)
+{
+    if(pNode->pLeft!=NULL)
+        listnodes(pNode->pLeft);
+    for (int j = 0; j < pNode->count; ++j)
+    {
+        printf("%s", pNode->item);
+    }
+    if (pNode->pRight!=NULL)
+    {
+        listnodes(pNode->pRight);
+    }
+}
+void freenodes(struct Node *pNode)
+{
+    if(pNode==NULL)
+        return;
+    if (pNode->pLeft!=NULL)
+    {
+        freenodes(pNode->pLeft);
+    }
+    if (pNode->pRight!=NULL)
+    {
+        freenodes(pNode->pRight);
+    }
+    free(pNode);
+}
 
 
