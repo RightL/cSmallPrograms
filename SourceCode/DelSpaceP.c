6e30f702f30712e8a17b1b4377b565fbb389f4e5
RightL <liu20001021@hotmail.com>
2014-12-24 13:11:59 +0800

    some small change

--- SourceCode/DelSpaceP.c
+++ SourceCode/DelSpaceP.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include <ctype.h>
 int main(void)
 {
-    
+    char string[50];
+    char *pstring=NULL;
+    pstring=string;
+    fgets(pstring,50,stdin);
+    int count=0;
+    for(int i = 0; i < 50; ++i)
+    {
+        count=i;
+        while(isspace(*(pstring+i))||ispunct(*(pstring+i)))
+        {
+            *(pstring+count)=*(pstring+(count+1));
+        }
+    } 
+    printf("删除空格和标点后的句子是：\n%s",pstring);
     return 0;
 }
 
--- SourceCode/PutSentencesLongShortSimple.cpp
+++ SourceCode/PutSentencesLongShortSimple.cpp
@@ -7,21 +7,47 @@
 int main(void)
 {
     char *pstring[MAX_SEN]={NULL};
-    bool keep=true;
-    printf("输入几个句子（在新行输入回车退出输入）：\n");
-    for (int i = 0; i < MAX_SEN; ++i)
+    printf("Type a sentence:\n");
+    int i;
+    for (i = 0; i < MAX_SEN; ++i)
     {
         pstring[i]=(char *)malloc(sizeof(char)*MAX_LEN);
-        if (*(pstring[i])=='\n')
+        if (pstring[i]==NULL)
+        {
+            printf("malloc \n");
+            exit(1);
+        }
+        fgets(pstring[i],MAX_LEN,stdin);
+        if (*(pstring[i]+0)=='\n')
         {
             free(pstring[i]);
             break;
         }
-        fgets(pstring[i],MAX_LEN,stdin);
-    }
-    while(keep)
-    {
         
+        fflush(stdin);
+    }
+    printf("Put sentence from Short to Long:\n");
+    for (int j = 0; j < i; ++j)
+    {
+        int min_index=j;
+        if (pstring[j]==NULL)
+        {
+            continue;
+        }
+        for (int q = j+1; q < i; ++q)
+        {
+            if (pstring[q]==NULL||pstring[min_index]==NULL)
+            {
+                continue;
+            }
+            if (strlen(pstring[q])<strlen(pstring[min_index]))
+            {
+                min_index=q;
+            }
+        }
+        printf("%s",pstring[min_index]);
+        free(pstring[min_index]);
+        pstring[min_index]=NULL;
     }
     return 0;
 }
