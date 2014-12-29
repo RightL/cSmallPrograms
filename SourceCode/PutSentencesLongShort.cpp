1e296897e8139a9c18de0a20c8e95e69b964b7fc
RightL <liu20001021@hotmail.com>
2014-12-29 13:03:09 +0800

    格式化代码

--- SourceCode/PutSentencesLongShort.cpp
+++ SourceCode/PutSentencesLongShort.cpp
@@ -7,18 +7,18 @@
 
 struct Node
 {
-  char item[MAX_LEN];
-  int count;
-  int string_lenth;
-  struct Node *pLeft;
-  struct Node *pRight;
+    char item[MAX_LEN];
+    int count;
+    int string_lenth;
+    struct Node *pLeft;
+    struct Node *pRight;
 };
 
 struct LinkedList
 {
-  struct LinkedList *pnext;
-  int string_lenth;
-  char string[MAX_LEN];
+    struct LinkedList *pnext;
+    int string_lenth;
+    char string[MAX_LEN];
 };
 struct Node *createnode (char string[MAX_LEN], int string_lenth);
 struct Node *addnode (char string[], struct Node *pNode, int string_lenth);
@@ -27,171 +27,170 @@ void listnodes (struct Node *pNode);
 static struct LinkedList *first = NULL;
 void freelinked (struct LinkedList *first);
 
-int
-main (void)
+int main (void)
 {
-  int tmpstring_lenth;
-  struct Node *pRoot = NULL;
-  bool needfree = false;
-  printf ("Type some sentences:\n");
-  while (true)
+    int tmpstring_lenth;
+    struct Node *pRoot = NULL;
+    bool needfree = false;
+    printf ("Type some sentences:\n");
+    while (true)
     {
-      tmpstring_lenth = 0;
-      char tmpstring[MAX_LEN] = {0};
-      fgets (tmpstring, MAX_LEN, stdin);
-      if (tmpstring[0] == '\n')
+        tmpstring_lenth = 0;
+        char tmpstring[MAX_LEN] = {0};
+        fgets (tmpstring, MAX_LEN, stdin);
+        if (tmpstring[0] == '\n')
         {
-          break;
+            break;
         }
-      tmpstring_lenth = (int) strlen (tmpstring);
-      if (pRoot == NULL)
+        tmpstring_lenth = (int) strlen (tmpstring);
+        if (pRoot == NULL)
         {
-          pRoot = createnode (tmpstring, tmpstring_lenth);
+            pRoot = createnode (tmpstring, tmpstring_lenth);
         }
-      else
+        else
         {
-          if (addnode (tmpstring, pRoot, tmpstring_lenth) == NULL)
+            if (addnode (tmpstring, pRoot, tmpstring_lenth) == NULL)
             {
-              needfree = true;
-              static struct LinkedList *current = NULL;
-              static struct LinkedList *previous = NULL;
-              current = (struct LinkedList*) malloc (sizeof (struct LinkedList));
-              if (first == NULL)
+                needfree = true;
+                static struct LinkedList *current = NULL;
+                static struct LinkedList *previous = NULL;
+                current = (struct LinkedList*) malloc (sizeof (struct LinkedList));
+                if (first == NULL)
                 {
-                  first = current;
+                    first = current;
                 }
-              if (previous != NULL)
+                if (previous != NULL)
                 {
-                  previous->pnext = current;
+                    previous->pnext = current;
                 }
-              current->string_lenth = tmpstring_lenth;
-              for (int i = 0; i < tmpstring_lenth; ++i)
+                current->string_lenth = tmpstring_lenth;
+                for (int i = 0; i < tmpstring_lenth; ++i)
                 {
-                  current->string[i] = tmpstring[i];
+                    current->string[i] = tmpstring[i];
                 }
-              current->pnext = NULL;
-              previous = current;
+                current->pnext = NULL;
+                previous = current;
             }
         }
-      fflush (stdin);
+        fflush (stdin);
     }
-  printf ("All the sentences are:\n");
-  listnodes (pRoot);
-  freenodes (pRoot);
-  if (needfree)
+    printf ("All the sentences are:\n");
+    listnodes (pRoot);
+    freenodes (pRoot);
+    if (needfree)
     {
-      freelinked (first);
+        freelinked (first);
     }
-  return 0;
+    return 0;
 }
 
-struct Node *
+    struct Node *
 createnode (char string[MAX_LEN], int string_lenth)
 {
-  struct Node *pNode = (struct Node *) malloc (sizeof (struct Node));
-  if (pNode == NULL)
+    struct Node *pNode = (struct Node *) malloc (sizeof (struct Node));
+    if (pNode == NULL)
     {
-      printf ("malloc faild\n");
-      exit (1);
+        printf ("malloc faild\n");
+        exit (1);
     }
-  for (int i = 0; i < (int) strlen (string); ++i)
+    for (int i = 0; i < (int) strlen (string); ++i)
     {
-      pNode->item[i] = string[i];
+        pNode->item[i] = string[i];
     }
-  pNode->count = 1;
-  pNode->pLeft = pNode->pRight = NULL;
-  pNode->string_lenth = string_lenth;
-  return pNode;
+    pNode->count = 1;
+    pNode->pLeft = pNode->pRight = NULL;
+    pNode->string_lenth = string_lenth;
+    return pNode;
 }
 
-struct Node *
+    struct Node *
 addnode (char string[], struct Node *pNode, int string_lenth)
 {
-  if (pNode == NULL)
+    if (pNode == NULL)
     {
-      return createnode (string, string_lenth);
+        return createnode (string, string_lenth);
     }
-  if (string_lenth == pNode->string_lenth)
+    if (string_lenth == pNode->string_lenth)
     {
-      ++pNode->count;
-      return NULL;
+        ++pNode->count;
+        return NULL;
     }
-  if (string_lenth < pNode->string_lenth)
+    if (string_lenth < pNode->string_lenth)
     {
-      if (pNode->pLeft == NULL)
+        if (pNode->pLeft == NULL)
         {
-          pNode->pLeft = createnode (string, string_lenth);
-          return pNode->pLeft;
+            pNode->pLeft = createnode (string, string_lenth);
+            return pNode->pLeft;
         }
-      else
+        else
         {
-          return addnode (string, pNode->pLeft, string_lenth);
+            return addnode (string, pNode->pLeft, string_lenth);
         }
     }
-  else
+    else
     {
-      if (pNode->pRight == NULL)
+        if (pNode->pRight == NULL)
         {
-          pNode->pRight = createnode (string, string_lenth);
-          return pNode->pRight;
+            pNode->pRight = createnode (string, string_lenth);
+            return pNode->pRight;
         }
-      else
+        else
         {
-          return addnode (string, pNode->pRight, string_lenth);
+            return addnode (string, pNode->pRight, string_lenth);
         }
     }
 }
 
-void
+    void
 listnodes (struct Node *pNode)
 {
-  if (pNode->pLeft != NULL)
-    listnodes (pNode->pLeft);
-  if (pNode->count != 1)
+    if (pNode->pLeft != NULL)
+        listnodes (pNode->pLeft);
+    if (pNode->count != 1)
     {
-      struct LinkedList *tmp = NULL;
-      tmp = first;
-      while (tmp != NULL)
+        struct LinkedList *tmp = NULL;
+        tmp = first;
+        while (tmp != NULL)
         {
-          if (tmp->string_lenth == pNode->string_lenth)
+            if (tmp->string_lenth == pNode->string_lenth)
             {
-              printf ("%s", tmp->string);
+                printf ("%s", tmp->string);
             }
-          tmp = tmp->pnext;
+            tmp = tmp->pnext;
         }
     }
-  printf ("%s", pNode->item);
-  if (pNode->pRight != NULL)
+    printf ("%s", pNode->item);
+    if (pNode->pRight != NULL)
     {
-      listnodes (pNode->pRight);
+        listnodes (pNode->pRight);
     }
 }
 
-void
+    void
 freenodes (struct Node *pNode)
 {
-  if (pNode == NULL)
-    return;
-  if (pNode->pLeft != NULL)
+    if (pNode == NULL)
+        return;
+    if (pNode->pLeft != NULL)
     {
-      freenodes (pNode->pLeft);
+        freenodes (pNode->pLeft);
     }
-  if (pNode->pRight != NULL)
+    if (pNode->pRight != NULL)
     {
-      freenodes (pNode->pRight);
+        freenodes (pNode->pRight);
     }
-  free (pNode);
+    free (pNode);
 }
 
-void
+    void
 freelinked (struct LinkedList *current)
 {
-  struct LinkedList *previous = NULL;
-  while (current == NULL)
+    struct LinkedList *previous = NULL;
+    while (current == NULL)
     {
-      previous = current;
-      current = current->pnext;
-      free (previous);
+        previous = current;
+        current = current->pnext;
+        free (previous);
     }
 }
 
