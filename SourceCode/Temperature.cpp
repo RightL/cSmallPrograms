e810d2378887e20b32e90a821e2a7d249f2d8d9d
RightL <liu20001021@hotmail.com>
2015-02-01 09:58:21 +0800

    overwrite input code

--- SourceCode/Temperature.cpp
+++ SourceCode/Temperature.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
+//用链表储存温度值
 struct LinkedList
 {
     struct LinkedList *pnext;
@@ -9,39 +11,42 @@ struct LinkedList
     int count_num;
 };
 
-float average_degree(float *InPutNumber);
+float average_degree(float *InPutNumber);       /*进行平均温度计算*/
 
 int main(void)
 {
     struct LinkedList *first=NULL;
     struct LinkedList *current=NULL;
     struct LinkedList *previous=NULL;
-    int keep=1;
+    bool keep=true;                             /*是否继续输入*/
 
     printf("Type some tmptemperature:\n");
-
+    printf("(must have six times a day)\n\n\n");
     while(true)
     {
+        //输入温度值
         printf("The next day:\n");
         float tmptemperature[6]={0};
-        int count_number=0;
         for (int i = 0; i < 6; ++i)
         {
-            keep=scanf("%f",&tmptemperature[i]);
-            if (keep)
+            //在第一天的第一个温度值输入一个字母或符号退出输入
+            if (i==0)
             {
-                count_number++;
+                keep=scanf("%f",&tmptemperature[i]);
+                if (!keep)
+                {
+                    break;    
+                }
             }
             else
             {
-                break;
+                scanf("%f",&tmptemperature[i]); 
             }
         }
         if (!keep)
         {
             break;
         }
-        
         current=(struct LinkedList*)calloc(6,sizeof(struct LinkedList));
         if (first==NULL)
         {
