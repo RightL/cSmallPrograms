c7312ee7c5e64bebf6d4d0ffbf03f25cb127ae5d
RightL <liu20001021@hotmail.com>
2015-02-02 11:54:12 +0800

    修改：     SourceCode/avagenum_func.cpp

--- SourceCode/avagenum_func.cpp
+++ SourceCode/avagenum_func.cpp
@@ -4,22 +4,22 @@
 #define STEP 4
 
 float* allocate_memory(float *data);
-float average_num(float *data);
+float average_num(float *data,size_t count_num);
+
+size_t count_num=0;
+size_t max_count_num=0;
 
 int main(void)
 {
     float *data=NULL;
-    size_t count_num=0;
-    size_t max_count_num=0;
 
     printf("Type some numbers:\nTYPE A LETTER TO THE END OF INPUT\n\n");
     while(true)
     {
         if (count_num==max_count_num)
         {
-            
-        }
-        data=allocate_memory(data); 
+            data=allocate_memory(data); 
+        } 
     }
     return 0;
 }
@@ -27,11 +27,33 @@ int main(void)
 //为输入的值分配内存
 float* allocate_memory(float *data)
 {
+    float *newdata=NULL;
+    max_count_num+=STEP;
+    //allocate_memory
+    newdata=(float*)calloc(1,max_count_num*sizeof(float)); 
+    if (newdata==NULL)
+    {
+        printf("failed to allocate_memory\n");
+        exit(1); 
+    }
+    //copy old data to new one
+    if (data!=NULL)
+    {
+        for (size_t i = 0; i < count_num; ++i)
+        {
+            *(newdata+i)=*(data+i);  
+        }
+    }
+    data=newdata;
+    free(newdata);
+    newdata=NULL; 
+
     return data;
 }
 //计算平均数
-float average_num(float *data)
+float average_num(float *data,size_t count_num)
 {
-    float average_num=0;
-    return average_num;
+    float total=0;
+    return total/count_num;
 }
+
