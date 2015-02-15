94e2f8b8deb5f3698a847bc5b7f980749c67e7d9
RightL <liu20001021@hotmail.com>
2015-02-15 12:08:06 +0800

    新文件：   SourceCode/numtostring.cpp

---
+++ SourceCode/numtostring.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+char *  numtostring(int number);
+
+int main(void)
+{
+    int number=0;
+
+    printf("\n: %s\n", numtostring(number));
+
+    return 0;
+}
+
+char *  numtostring(int number)
+{
+    char *pstring=NULL;
+
+    return pstring;
+}
