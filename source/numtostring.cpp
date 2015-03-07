d26f71c08afac92c5260f57fd30c3010e7e77f23
RightL <liu20001021@hotmail.com>
2015-03-07 21:30:30 +0800

    Finish numtostring.cpp

--- source/numtostring.cpp
+++ source/numtostring.cpp
@@ -1,29 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define MAX_STRING_LEN 10               /*The int32_t has 10*/
+/*int is 19*/
+#define MAX_STRING_LEN 19
 
-char* numtostring(int number);
+char *numtostring(int number);
 
 int main(void)
 {
-	int32_t number=0;
+	printf("\nType a number: ");
+	int number=0;
+	char *numstr = NULL;
 
-	printf("\n: %s\n", numtostring(number));
+	scanf("%d", &number);
 
+	numstr = numtostring(number);
+	if (!numstr) {
+		printf("Unexpected error\n");
+		return -1;
+	}
+
+	printf("The string is : %s\n\n", numstr);
+
+	free(numstr);
 	return 0;
 }
 
-char* numtostring(int32_t number)
+char *numtostring(int number)
 {
-	char *pstring=NULL;
+	// '\0' + '-' = 2
+	char numstr[MAX_STRING_LEN + 2] = {0};
 
 	bool isnegative = number < 0;
 	if (isnegative)
-		number = -number;                   /*make it mositive*/
-	do{
-		int8_t tmpnum = number%10;
+		number = -number;
+
+	unsigned int i = 0;
+	do {
+		if (i > MAX_STRING_LEN) {
+			printf("Too long number:%d\n", i);
+			return NULL;
+		}
+
+		int8_t tmpnum = static_cast<int8_t>(number%10);
+		number = number / 10;
+		numstr[i] = static_cast<char>('0' + tmpnum);
+
+		++i;
 	} while(number != 0);
 
-	return pstring;
+	if (isnegative) {
+		numstr[i + 1] = '-';
+		numstr[i + 2] = '\0';
+	} else {
+		numstr[i + 1] = '\0';
+	}
+
+	size_t numstr_len = strlen(numstr) + 1;
+	char *pnumstr = reinterpret_cast<char *>(malloc(numstr_len));
+	if (!pnumstr) {
+		printf("Memery allocation failure\n");
+		return NULL;
+	}
+
+	strcpy(pnumstr, numstr);
+	return pnumstr;
 }
