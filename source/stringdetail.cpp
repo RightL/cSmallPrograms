f274c9a35d09d6d7d285f29e2aef7ce5065d686f
RightL <liu20001021@hotmail.com>
2015-03-26 14:03:49 +0800

    简单框架 新文件:       source/stringdetail.cpp

---
+++ source/stringdetail.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+
+int string_words_count(char *string);
+char *to_words(char *string, char *words);
+int letter_count(char *string);
+bool is_separator(char character);
+
+int main(int argc, char const *argv[])
+{
+        printf("Type a paragraph:\n\n");
+
+        return 0;
+}
+
+// How many words in the string
+int string_words_count(char *string)
+{
+        int  string_words_count = 0;
+        return string_words_count;
+}
+
+// Turn paragraph to words
+char *to_words(char *sting, char *words)
+{
+        return words;
+}
+
+// How many letter in the string
+int letter_count(char *string)
+{
+        int letter_count = 0;
+        return letter_count;
+}
+
+// Is separator
+bool is_separator(char character)
+{
+        bool is_separator = false;
+        return is_separator;
+}
