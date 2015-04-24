4fb11cbb15c7dc4132281d0447e7b57d743ee542
RightL <liu20001021@hotmail.com>
2015-04-24 13:34:21 +0800

    修改:         source/arrayOp.c

--- source/arrayOp.c
+++ source/arrayOp.c
@@ -2,12 +2,15 @@
 
 double add(double a, double b);
 double subtract(double a, double b);
-double multiply(double a , double b);
+double multiply(double a, double b);
 double array_op(double array[], size_t size, double (*pfun)(double, double));
 
 
 int main(void)
 {
+        double array[] = {1,2,3,4,5,6,7};
+        size_t lenth = sizeof(array) / sizeof(double);
+        printf("%f\n", array_op(array, lenth, subtract));
         return 0;
 }
 
@@ -21,12 +24,15 @@ double subtract(double a, double b)
         return a - b;
 }
 
-double multiply(double a , double b)
+double multiply(double a, double b)
 {
         return a * b;
 }
 
 double array_op(double array[], size_t size, double (*pfun)(double, double))
 {
-        return size;
+        double result = array[size - 1];
+        for (size_t i = size - 1; i > 0; i--)
+                result = pfun(array[i -1], result);
+        return result;
 }
