7261ce43ce21f1bb636d4a1a5513229c3a6242f7
RightL <liu20001021@hotmail.com>
2015-04-24 18:31:30 +0800

    修改:         test/strtko.c

--- test/strtko.c
+++ test/strtko.c
@@ -8,6 +8,5 @@ int main(int argc, char const *argv[]) {
         pWord[0] = strtok(a, delim);
         printf("%zu\n", strlen(pWord[0]));
         printf("%s\n", pWord[0]);
-        free(pWord[0]);
         return 0;
 }
