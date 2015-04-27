e3ce8733db8ac9e924ca9569b4142042cf6d96b8
RightL <liu20001021@hotmail.com>
2015-04-27 21:02:21 +0800

    修改:         source/connectStr.c

--- source/connectStr.c
+++ source/connectStr.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-char *join_strings(char *strings[]);
+char *join_strings(char *strings[], size_t strCount);
 char *read_string(char terminator);
 
 int main(void) {
         char terminator = '*';
         char answer = 'y';
         char *pStrings[] = {NULL};
+        char *pStr = NULL;
+        size_t strCount = 0;
+
         printf("Type something\n");
+
+        for (size_t i = 0; i < strCount; i++)
+                free(pStrings[i]);
+        free(pStr);
         return 0;
 }
 
-char *join_strings(char *strings[], size_t strLenth) {
+char *join_strings(char *strings[], size_t strCount) {
         char *pStr = NULL;
-        if (pStr = (char *)malloc(sizeof(char) * strLenth)) {
-                printf("Memery allocation failure\n");
-                exit(-1);
-        }
-        
+        size_t strLen = 0;
+
+        for (size_t i = 0; i < strCount; i++)
+                strLen += strlen(strings[i]);
+        pStr = (char *)malloc(sizeof(char) * strLen);
+
         return pStr;
 }
 
