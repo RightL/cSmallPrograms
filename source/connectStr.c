b8b27833736553b61fc28f332125da53d323a82a
RightL <liu20001021@hotmail.com>
2015-04-29 20:07:19 +0800

    修改:         source/connectStr.c

--- source/connectStr.c
+++ source/connectStr.c
@@ -5,6 +5,8 @@
 char *join_strings(char *strings[], size_t strCount);
 char *read_string(char term_end);
 
+#define BUFFER_SIZE 50
+
 static const char terminator = '*';
 static const char answer = 'y';
 
@@ -24,11 +26,17 @@ int main(void) {
 
 char *join_strings(char *strings[], size_t strCount) {
         char *pStr = NULL;
-        size_t strLen = 0;
+        size_t total_lenth = 0;
+        size_t lenth = 0;
 
-        for (size_t i = 0; i < strCount; i++)
-                strLen += strlen(strings[i]);
-        pStr = (char *)malloc(sizeof(char) * strLen);
+        for (size_t i = 0; i < strCount; i++) {
+                total_lenth += strlen(strings[i]);
+                if (strings[i][lenth - 1] != '\n') {
+                        total_lenth++;
+                }
+        }
+
+        pStr = (char *)malloc(sizeof(char) * total_lenth);
         if (pStr == NULL) {
                 printf("Memery allocation failure\n");
                 exit(-1);
@@ -36,12 +44,25 @@ char *join_strings(char *strings[], size_t strCount) {
 
         for (size_t i = 0; i < strCount; i++) {
                 strcat(pStr, strings[i]);
+                lenth = strlen(strings[i]);
+                if (strings[i][lenth - 1] != '\n') {
+                        strings[i][lenth] = '\n';
+                        strings[i][lenth + 1] ='\0';
+                }
         }
 
         return pStr;
 }
 
 char *read_string(char term_end) {
+        char *str = NULL;
+        size_t i = 0;
+
+        do {
+                str = (char *)malloc(sizeof(char) * BUFFER_SIZE);
+                i++;
+        } while(term_end != (*(str + i) = (char)getchar()));
+
 
         return 0;
 }
