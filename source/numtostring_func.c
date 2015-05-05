76d4df5feafdb4aa99377ee7b1629c0e0e7df39b
RightL <liu20001021@hotmail.com>
2015-05-05 19:46:57 +0800

    修改:         source/connectStr.c

---
+++ source/numtostring_func.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+char *to_string(size_t count, double first, ...);
+
+int main(void)
+{
+
+        return 0;
+}
+
+char *to_string(size_t count, double first, ...)
+{
+        char *string = NULL;
+        return string;
+}
